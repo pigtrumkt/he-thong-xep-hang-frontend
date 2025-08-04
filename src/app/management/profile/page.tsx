05bc5f24e2309edde592e2ed75d5677f5020f977
pigtrum <pigtrumkt@gmail.com>
2025-08-04 14:45:58 +0700

    add

--- src/app/management/profile/page.tsx
+++ src/app/management/profile/page.tsx
@@ -273,6 +273,20 @@ export default function ProfilePage() {
             />
           </div>
 
+          {/* Cơ quan */}
+          {![1, 2].includes(user.role_id) && (
+            <div>
+              <label className="block mb-1 font-medium text-gray-600">
+                Cơ quan
+              </label>
+              <input
+                disabled
+                className="w-full px-4 py-2 bg-gray-100 border border-gray-300 rounded-lg text-slate-700 disabled:bg-gray-100"
+                value={user.agency_name}
+              />
+            </div>
+          )}
+
           {/* Họ và tên */}
           <div>
             <label className="block mb-1 font-medium text-gray-600">
