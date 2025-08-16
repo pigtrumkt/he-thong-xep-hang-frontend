1e0da7720de9901cfa446d4d35980b450f96213c
pigtrum <pigtrumkt@gmail.com>
2025-08-17 04:52:48 +0700

    add

--- src/app/management/agency/counters-screen-advertisement-management/page.tsx
+++ src/app/management/agency/counters-screen-advertisement-management/page.tsx
@@ -25,26 +25,26 @@ export default function CountersScreenAdvertisementManagementPage() {
   };
 
   return (
-    <div className="bg-white border border-blue-200 shadow-xl rounded-3xl p-6 mx-4 my-6 lg:min-w-[55rem]">
-      <div className="mx-auto">
-        <div className="relative z-10">
+    <>
+      <div className="bg-white border border-blue-200 shadow-xl rounded-3xl p-6 mx-4 mt-6 lg:min-w-[55rem]">
+        <div className="relative z-10 mx-auto">
           {/* Mode Selection */}
-          <div className="grid grid-cols-3 gap-2 mb-8 ">
+          <div className="grid grid-cols-3 gap-6">
             {/* No Ads */}
             <button
               onClick={() => handleChangeMode(0)}
-              className={`group relative overflow-hidden rounded-2xl p-4 text-left border-2 transition-all duration-300 transform ${
+              className={`group relative overflow-hidden rounded-xl p-4 text-left border-1 transform ${
                 mode === 0
-                  ? "bg-gradient-to-br from-gray-500 to-slate-600 border-gray-400 text-white "
-                  : "bg-white/80 border-blue-200 hover:border-blue-400 text-gray-700"
+                  ? "bg-gradient-to-br bg-blue-500 to-indigo-600 border-blue-400 text-white shadow-xl"
+                  : "bg-white/80 border-blue-300 hover:border-blue-600 text-gray-700"
               }`}
             >
               <div className="flex items-center gap-4">
                 <div
-                  className={`p-3 rounded-xl shadow-lg transition-all duration-300 ${
+                  className={`p-3 rounded-xl shadow-lg  ${
                     mode === 0
                       ? "bg-white/20 text-white"
-                      : "bg-gradient-to-br from-gray-500 to-slate-600 text-white"
+                      : "bg-gradient-to-br from-blue-500 to-indigo-600 text-white"
                   }`}
                 >
                   <svg
@@ -65,15 +65,15 @@ export default function CountersScreenAdvertisementManagementPage() {
             {/* Image */}
             <button
               onClick={() => handleChangeMode(1)}
-              className={`group relative overflow-hidden rounded-2xl p-4 text-left border-2 transition-all duration-300 transform ${
+              className={`group relative overflow-hidden rounded-xl p-4 text-left border-1  transform ${
                 mode === 1
-                  ? "bg-gradient-to-br bg-blue-500 to-indigo-600 border-blue-400 text-white "
-                  : "bg-white/80 border-blue-200 hover:border-blue-400 text-gray-700"
+                  ? "bg-gradient-to-br bg-blue-500 to-indigo-600 border-blue-400 text-white shadow-xl"
+                  : "bg-white/80 border-blue-300 hover:border-blue-400 text-gray-700"
               }`}
             >
               <div className="flex items-center gap-4">
                 <div
-                  className={`p-3 rounded-xl shadow-lg transition-all duration-300 ${
+                  className={`p-3 rounded-xl shadow-lg ${
                     mode === 1
                       ? "bg-white/20 text-white"
                       : "bg-gradient-to-br from-blue-500 to-indigo-600 text-white"
@@ -98,15 +98,15 @@ export default function CountersScreenAdvertisementManagementPage() {
             {/* Video */}
             <button
               onClick={() => handleChangeMode(2)}
-              className={`group relative overflow-hidden rounded-2xl p-4 text-left border-2 transition-all duration-300 transform ${
+              className={`group relative overflow-hidden rounded-xl p-4 text-left border-1 transform ${
                 mode === 2
-                  ? "bg-gradient-to-br from-blue-500 to-indigo-600 border-blue-400 text-white "
-                  : "bg-white/80 border-blue-200 hover:border-blue-400 text-gray-700"
+                  ? "bg-gradient-to-br from-blue-500 to-indigo-600 border-blue-400 text-white shadow-xl"
+                  : "bg-white/80 border-blue-300 hover:border-blue-400 text-gray-700"
               }`}
             >
               <div className="flex items-center gap-4">
                 <div
-                  className={`p-3 rounded-xl shadow-lg transition-all duration-300 ${
+                  className={`p-3 rounded-xl shadow-lg ${
                     mode === 2
                       ? "bg-white/20 text-white"
                       : "bg-gradient-to-br from-blue-500 to-indigo-600 text-white"
@@ -127,7 +127,10 @@ export default function CountersScreenAdvertisementManagementPage() {
               </div>
             </button>
           </div>
-
+        </div>
+      </div>
+      <div className="bg-white border border-blue-200 shadow-xl rounded-3xl p-6 pt-8 mx-4 mt-4 lg:min-w-[55rem]">
+        <div className="relative z-10 mx-auto">
           {/* Uploads + Preview */}
           {mode === 0 && <NoAdvertisementComponent></NoAdvertisementComponent>}
           {mode === 1 && (
@@ -152,6 +155,6 @@ export default function CountersScreenAdvertisementManagementPage() {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
