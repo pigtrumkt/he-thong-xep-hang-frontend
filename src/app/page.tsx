881891b2da001f946d9fa5b49b8a60e6d7e446c1
pigtrumkt <pigtrumkt@gmail.com>
2025-09-09 22:20:47 +0700

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/login"); // 👈 chuyển hướng
-  }, [router]);
-
   return (
-    <div className="flex h-screen items-center justify-center text-gray-500 text-sm">
-      Đang chuyển hướng...
-    </div>
+    <div className="flex h-screen items-center justify-center text-gray-500 text-sm"></div>
   );
 }
